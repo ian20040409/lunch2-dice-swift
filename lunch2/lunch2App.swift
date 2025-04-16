492ebe6b9ceae9190b6a27c66fe8693f3b1e7902
You <78723629+ian20040409@users.noreply.github.com>
2025-04-17 05:16:09 +0800

    Initial Commit

---
+++ lunch2/lunch2App.swift
@@ -0,0 +1,17 @@
+//
+//  lunch2App.swift
+//  lunch2
+//
+//  Created by 林恩佑 on 2025/4/17.
+//
+
+import SwiftUI
+
+@main
+struct lunch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lunch2Tests/lunch2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  lunch2Tests.swift
+//  lunch2Tests
+//
+//  Created by 林恩佑 on 2025/4/17.
+//
+
+import Testing
+@testable import lunch2
+
+struct lunch2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
